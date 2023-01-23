f908e56bce50673984d9a240a21320faa3a3cc41
auto-farm-store <115274419+auto-farm-store@users.noreply.github.com>
2023-01-23 20:33:24 +0530

    Added buttons footer and some styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	import Navbar from '../lib/Navbar.svelte';
+	import darrow from '../lib/assets/darrow.png';
 
-	let speed = 3;
+	let speed = 2;
+	let speed2 = 4;
 	let scroll;
 </script>
 
@@ -17,17 +19,26 @@
 	>
 		<Navbar />
 		<div class="" />
-		<h2 class="text-red-500 font-bold text-5xl px-[5%]">We build awesome <br />software</h2>
+		<h2 class="text-red-500 font-bold text-5xl px-[5%]"><span class="text-white">We build</span> awesome <br/> software!</h2>
 	</div>
-	<div class="h-[50vh] bg-black" style:transform={`translate3d(0,${scroll * speed}px,0)`}>
+	<div class="h-[50vh] bg-black relative" style:transform={`translate3d(0,${scroll * speed2}px,0)`}>
 		<h2 class="text-white text-2xl">
-			<h2 class="text-white text-4xl px-[5%] md:pr-[50%]">
-				Most of today's problems have software solutions. We help you find and execute them - in
+			<h2 class="text-white text-2xl pt-8 px-[5%] md:pr-[50%]">
+				Most of today's problems have software <br/> solutions. We help you find and execute <br/> them - in
 				style.
 			</h2>
 		</h2>
+		<div class="absolute mt-0 ml-0">
+			<button class=" bg-transparent border-white w-64 border-[1px] py-2  ml-20 mt-10 px-5 text-white absolute z-10">Get Talent</button>
+			<button class=" bg-buttonRed  w-64 h-10 py-2  ml-[34.5%] mt-[17.8%] px-5 text-white relative z-1"></button>
+		</div>
+		<div class="mt-44 ml-[44%] text-gray-200 text-opacity-60 content flex pl-2">
+		<img src={darrow} alt="arrow" class="w-4 mt-2 h-4"/>
+			<span class="ml-2 font-normal">scroll down to know more</span>
+	</div>
 	</div>
 </div>
 
 <style>
+
 </style>
